73189086d833819826ed51e6917b166e789af9b0
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-03-25 23:03:09 +0900

    Time: 66 ms (45.29%), Space: 14.4 MB (91.29%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,17 +1,17 @@
 class Solution:
     # row * col 값에서 binary search하는 방식
-    def searchMatrix2(self, matrix: List[List[int]], target: int) -> bool:
-        col = len(matrix[0])
-        start, end = 0, len(matrix) * len(matrix[0]) - 1
-        while start <= end:
-            mid = (start + end) // 2
-            if matrix[mid // col][mid % col] == target:
-                return True
-            elif matrix[mid // col][mid % col] < target:
-                start = mid + 1
-            else:
-                end = mid - 1
-        return False
+    # def searchMatrix2(self, matrix: List[List[int]], target: int) -> bool:
+    #     col = len(matrix[0])
+    #     start, end = 0, len(matrix) * len(matrix[0]) - 1
+    #     while start <= end:
+    #         mid = (start + end) // 2
+    #         if matrix[mid // col][mid % col] == target:
+    #             return True
+    #         elif matrix[mid // col][mid % col] < target:
+    #             start = mid + 1
+    #         else:
+    #             end = mid - 1
+    #     return False
     
     # row와 col값을 각각 binary search하는 방식 
     def binary_search(self, row, target):
