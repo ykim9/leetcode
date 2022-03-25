782245a95afbdd20118de5dbf616dc044359709c
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-03-25 23:02:54 +0900

    Time: 79 ms (23.00%), Space: 14.5 MB (15.28%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,5 +1,6 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    # row * col 값에서 binary search하는 방식
+    def searchMatrix2(self, matrix: List[List[int]], target: int) -> bool:
         col = len(matrix[0])
         start, end = 0, len(matrix) * len(matrix[0]) - 1
         while start <= end:
@@ -11,3 +12,34 @@ class Solution:
             else:
                 end = mid - 1
         return False
+    
+    # row와 col값을 각각 binary search하는 방식 
+    def binary_search(self, row, target):
+        start, end = 0, len(row) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if row[mid] == target:
+                return True
+            elif row[mid] < target:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return False
+    
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        start, end = 0, len(matrix) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if matrix[mid][0] == target:
+                return True
+            elif matrix[mid][0] < target and (mid < len(matrix) - 1 and target < matrix[mid + 1][0]) \
+                or mid == len(matrix) - 1:
+                    return self.binary_search(matrix[mid], target)
+            elif matrix[mid][0] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return False
+    
+    
+    
