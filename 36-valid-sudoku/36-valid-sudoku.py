af8b8247fb88703da51ce3fc712a8aee39d498ce
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-03-25 21:04:03 +0900

    Time: 130 ms (56.66%), Space: 13.9 MB (87.94%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,27 +1,25 @@
 class Solution:
+    def isValidArray(self, array):
+        array = [i for i in array if i != "."]
+        return len(set(array)) == len(array)
+    
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
         # check for row
         for row in board:
-            if any(row.count(i) > 1 for i in row if i != "."):
+            if not self.isValidArray(row):
                 return False
         
         # check for column
         for col in zip(*board):
-            if any(col.count(i) > 1 for i in col if i != "."):
+            if not self.isValidArray(col):
                 return False
         
         # check for 3*3 sub-boxes
-        for k in range(0, 9, 3):
-            r = board[:k+3]
-            subBox = [[], [], []]
-            for i in range(k, k+3):
-                for j in range(3):
-                    subBox[j] += r[i][j*3:j*3+3]
-            
-            for b in subBox:
-                if any(b.count(i) > 1 for i in b if i != "."):
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                subBox = [board[x][y] for x in range(i, i+3) for y in range(j, j+3)]        
+                if not self.isValidArray(subBox):
                     return False
-            
+        
         return True
         
