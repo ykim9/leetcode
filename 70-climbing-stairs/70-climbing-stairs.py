f382ac9ac9082de1d5fe354565b21dad509a486d
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-03-30 00:43:23 +0900

    Time: 58 ms (13.69%), Space: 13.8 MB (62.09%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,15 +2,15 @@ class Solution:
     # 1번째는 1가지, 2번째는 2가지, 3번째는 3가지, 4번째는 5가지, 5번째는 8가지...
     # ways[n-1] + ways[n-2]가 정답
     # 방법 1
-    def climbStairs1(self, n: int) -> int:
-        if n <= 3:
-            return n
-        ways = [i+1 for i in range(n)]
+#     def climbStairs1(self, n: int) -> int:
+#         if n <= 3:
+#             return n
+#         ways = [i+1 for i in range(n)]
         
-        for i in range(3, n):
-            ways[i] = ways[i - 1] + ways[i - 2]
+#         for i in range(3, n):
+#             ways[i] = ways[i - 1] + ways[i - 2]
             
-        return ways[-1]
+#         return ways[-1]
     
     # 방법 2
     def climbStairs(self, n: int) -> int:
