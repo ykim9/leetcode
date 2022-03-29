60a788286875acc0d4e707dff1a61f6f32dcef12
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-03-30 01:31:39 +0900

    Time: 45 ms (52.73%), Space: 13.9 MB (70.63%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,17 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        max_rob = 0
-        ans = [0] * len(nums)
+        if len(nums) == 1: 
+            return nums[0]
+        dp = nums[:]
+        dp[1] = max(dp[0], dp[1])
         
-        for i in range(len(nums)):
-            if i < 2:
-                max_rob = max(max_rob, nums[i])
-                ans[i] = max_rob
-                continue
-            
+        for i in range(2, len(nums)):
             # 지금까지 강도 시나리오에서 제일 최고 이익이 되는 쪽과
             # 현재 위치 + 하나 떨어진 곳에서 얻은 최고 이익을 비교하여 높은 쪽 = 신규로 최고 이익 시나리오가 됨
-            max_rob = max(max_rob, nums[i] + ans[i - 2])
-            ans[i] = max_rob
+            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2])
             
-        return max_rob
+        return dp[-1]
