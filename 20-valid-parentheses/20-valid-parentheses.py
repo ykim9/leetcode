5b08bf6b68b3e089286364e6d9f766e616b8c924
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-03-29 23:27:57 +0900

    Time: 52 ms (33.46%), Space: 13.9 MB (74.56%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,16 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+        dic = {")":"(", "]":"[", "}":"{"}
         for p in s:
-            if p in ("(", "[", "{"):         # 여는 괄호는 stack에 push
-                stack.append(p)
-            elif p in (")", "]", "}"):
-                if not stack:
+            if p in dic:                               # 닫는 괄호일 경우
+                # stack이 비었거나 stack의 pop값과 dict[p]가 일치하지 않으면 False
+                if not stack or dic[p] != stack.pop():   
                     return False
-                last = stack.pop()           # stack에서 pop한 원소와 닫는 괄호 모양이 일치하지 않으면 False 
-                if (p == ")" and last != "(") or (p == "]" and last != "[") or (p == "}" and last != "{"):
-                    return False
-            else:
-                continue
-        return False if len(stack) else True
-                    
+            else:                                       # 여는 괄호일 경우, 혹은 그외 닫는 괄호가 아닌 경우
+                stack.append(p)                         # stack에 push
+        
+        return len(stack) == 0                          # stack에 모든 값이 pop되지 않은 경우는 False
+        
