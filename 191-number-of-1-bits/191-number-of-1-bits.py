3f4ecd3778f15db3c0f5d3533921b9b3586d3e04
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-03-30 18:13:23 +0900

    Time: 47 ms (45.76%), Space: 14 MB (9.17%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,3 +1,13 @@
 class Solution:
+    #방법 1: 비트연산 n&(n-1)은 가장 오른쪽 1값을 0으로 하는 것을 이용
     def hammingWeight(self, n: int) -> int:
+        answer = 0
+        while n:
+            n &= n - 1
+            answer += 1
+        return answer
+        
+    
+    #방법 2: binary를 string으로 바꿔서 1의 갯수 세기
+    def hammingWeight2(self, n: int) -> int:
         return str(bin(n)).count("1")
