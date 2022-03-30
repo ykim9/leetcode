7d9927d6c9d46ff96c846327fc2f15a834915558
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-03-30 22:29:25 +0900

    Time: 28 ms (95.42%), Space: 13.9 MB (64.10%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
@@ -7,7 +7,7 @@
 class Solution:
     # 후위 순회: left -> right -> root 순
     # 방법 1: recursive
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def postorderTraversal1(self, root: Optional[TreeNode]) -> List[int]:
         result = []
         def postorder( node):
             if not node:
@@ -18,3 +18,17 @@ class Solution:
         postorder(root)
         return result
     
+    # 방법 2: iterative
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()
+            if not node:
+                continue
+            if visited:
+                res.append(node.val)
+            else:
+                stack.append((node, True))
+                stack.append((node.right, False))
+                stack.append((node.left, False))
+        return res
