9b4a2bc46f728e3204d62ca08efc81b64924c542
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-06 10:30:42 +0900

    Time: 6232 ms (12.58%), Space: 21.9 MB (11.82%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,19 +1,21 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res = ""
         dp = [[False] * len(s) for _ in range(len(s))] # 예 2:3번째 글자가 Palindrome이면True가됨
         for i in range(len(s)):
             dp[i][i] = True
-            res = s[i]
+        
+        p_longest_start, p_longest_len = 0, 1
             
-        for i in range(len(s)-1,-1,-1):
-            for j in range(i+1, len(s)):
+        for end in range(0, len(s)):
+            for start in range(end-1,-1,-1):
                 # i번째와 j번째 char가 같고, j와 i 인덱스 차이가 하나이면 palindrome임
                 # i번째와 j번째 char가 같고, (i+1, j-1)번째 글자가 palindrome이면 True 
                 #  예. 1:1과 4:4가 같은데 2:3번째 범위 글자가Palindrome이면 Palindrome (1+1, 4-3) 
-                if s[i] == s[j]:
-                    if i+1 == j or dp[i+1][j-1]:
-                        dp[i][j] = True
-                        if len(res) < len(s[i:j+1]):
-                            res = s[i:j+1]
-        return res
+                if s[start] == s[end]:
+                    if start+1 == end or dp[start+1][end-1]:
+                        dp[start][end] = True
+                        palindrome_len = end - start + 1
+                        if p_longest_len < palindrome_len:
+                            p_longest_start = start
+                            p_longest_len = palindrome_len
+        return s[p_longest_start:p_longest_start+p_longest_len]
