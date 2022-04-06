c6f04fa5acae5d222786e547eafe606ffb5dd362
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-06 22:55:56 +0900

    Time: 899 ms (37.03%), Space: 16 MB (44.25%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,31 +1,17 @@
 class Solution:
-    def numEnclaves(self, A):
-        visited = set()
-        result = 0
-        for i in range(len(A)):
-            for j in range(len(A[0])):
-                if A[i][j] == 1 and (i, j) not in visited:
-                    temp = self.dfs(A, visited, i, j)
-                    if temp > 0:
-                        result += temp
-        return result
-
-    def dfs(self, A, visited, i, j):
-        if i < 0 or i == len(A) or j < 0 or j == len(A[0]):
-            return -1
-        if A[i][j] == 0:
-            return 0
-        if (i, j) in visited:
-            return 0
-        visited.add((i, j))
-        total = 1
-        top = self.dfs(A, visited, i-1, j)
-        bottom = self.dfs(A, visited, i+1, j)
-        left = self.dfs(A, visited, i, j-1)
-        right = self.dfs(A, visited, i, j+1)
-        if top == -1 or bottom == -1 or left == -1 or right == -1:
-            return -1
-        return total + top + bottom + left + right
+    # 외곽에 인접한 땅만 dfs로 방문해 모두 0으로 변경 후 육지의 수 세기
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        def dfs(i, j):
+            grid[i][j] = 0
+            for x, y in (i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1):
+                if 0 <= x < m and 0 <= y < n and grid[x][y]:
+                    dfs(x, y)
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and (i == 0 or j == 0 or i == m - 1 or j == n - 1):
+                    dfs(i, j)
+        return sum(sum(row) for row in grid)
     
 #     def numEnclaves(self, grid: List[List[int]]) -> int:
 #         m, n = len(grid), len(grid[0])
