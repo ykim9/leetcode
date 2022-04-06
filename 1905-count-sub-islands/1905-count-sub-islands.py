e7f71d803d72d73c95c39ac479ff8c261d0df92b
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-07 00:14:02 +0900

    Time: 4156 ms (66.68%), Space: 108 MB (19.56%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -3,23 +3,14 @@ class Solution:
         def dfs(x, y):
             if x in (-1, m) or y in (-1, n) or grid2[x][y] == 0:
                 return 1
-            
-            isSub = True
-            if grid1[x][y] == 0 and grid2[x][y] == 1:
-                isSub = False
-                #keep sink the island
-                
+            # grid1이 0이면(grid2는 현재 1) sub Islands False 1이면 True
+            isSub = grid1[x][y]
             grid2[x][y] = 0
             for i, j in ((x+1, y), (x-1, y), (x, y+1), (x, y -1)):
                 isSub *= dfs(i, j)
             return isSub
         
         m, n = len(grid1), len(grid1[0])
-        ans = 0
-        for x in range(m):
-            for y in range(n):
-                if grid1[x][y] == 1 and grid2[x][y] == 1:
-                    if dfs(x, y):
-                        ans += 1
-        return ans
+        return sum(dfs(x, y) for x in range(m) for y in range(n) if grid1[x][y] and grid2[x][y])
+
                     
