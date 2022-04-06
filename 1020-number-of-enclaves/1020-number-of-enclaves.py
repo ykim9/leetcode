908c64e0353f506b583be7483d14e9e016164d1b
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-06 22:53:58 +0900

    Time: 1394 ms (6.59%), Space: 19 MB (14.58%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,29 +1,31 @@
 class Solution:
-    def dfs(self, grid, visited, i, j):
-        if i < 0 or i == len(grid) or j < 0 or j == len(grid[0]):
+    def numEnclaves(self, A):
+        visited = set()
+        result = 0
+        for i in range(len(A)):
+            for j in range(len(A[0])):
+                if A[i][j] == 1 and (i, j) not in visited:
+                    temp = self.dfs(A, visited, i, j)
+                    if temp > 0:
+                        result += temp
+        return result
+
+    def dfs(self, A, visited, i, j):
+        if i < 0 or i == len(A) or j < 0 or j == len(A[0]):
             return -1
-        if grid[i][j] == 0:
+        if A[i][j] == 0:
             return 0
         if (i, j) in visited:
             return 0
         visited.add((i, j))
-        result = [self.dfs(grid, visited, x, y) for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]]
-        for r in result:
-            if r == -1:
-                return r
-        return 1 + sum(result)
-        
-    def numEnclaves(self, grid: List[List[int]]) -> int:
-        visited = set()
-        result = 0
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1 and (i, j) not in visited:
-                    temp = self.dfs(grid, visited, i, j)
-                    if temp > 0:
-                        result += temp
-        return result
+        total = 1
+        top = self.dfs(A, visited, i-1, j)
+        bottom = self.dfs(A, visited, i+1, j)
+        left = self.dfs(A, visited, i, j-1)
+        right = self.dfs(A, visited, i, j+1)
+        if top == -1 or bottom == -1 or left == -1 or right == -1:
+            return -1
+        return total + top + bottom + left + right
     
 #     def numEnclaves(self, grid: List[List[int]]) -> int:
 #         m, n = len(grid), len(grid[0])
