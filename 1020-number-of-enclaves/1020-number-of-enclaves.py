a3efc6099dace1aeca1f5f2079aa7eee72651333
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-06 22:53:33 +0900

    Time: 1583 ms (5.00%), Space: 19.5 MB (8.45%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,27 +1,52 @@
 class Solution:
-    def numEnclaves(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        def dfs(i, j):
-            if 0 > i or m <= i or 0 > j or n <= j:
-                return (0, 0)
-            elif grid[i][j] == 0:
-                return (1, 0)
-            
-            isEnclave, cnt = 1, 1
-            grid[i][j] = 0
-            
-            for x, y in ((i-1, j), (i+1, j), (i, j-1), (i, j+1)):
-            
-                result = dfs(x, y)
-                isEnclave *= result[0]
-                cnt += result[1]
-            return (isEnclave, cnt)
+    def dfs(self, grid, visited, i, j):
+        if i < 0 or i == len(grid) or j < 0 or j == len(grid[0]):
+            return -1
+        if grid[i][j] == 0:
+            return 0
+        if (i, j) in visited:
+            return 0
+        visited.add((i, j))
+        result = [self.dfs(grid, visited, x, y) for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]]
+        for r in result:
+            if r == -1:
+                return r
+        return 1 + sum(result)
         
-        ans = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    isEnclave, cnt = dfs(i, j)
-                    if isEnclave:
-                        ans += cnt
-        return ans
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        visited = set()
+        result = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1 and (i, j) not in visited:
+                    temp = self.dfs(grid, visited, i, j)
+                    if temp > 0:
+                        result += temp
+        return result
+    
+#     def numEnclaves(self, grid: List[List[int]]) -> int:
+#         m, n = len(grid), len(grid[0])
+#         def dfs(i, j):
+#             if 0 > i or m <= i or 0 > j or n <= j:
+#                 return (0, 0)
+#             elif grid[i][j] == 0:
+#                 return (1, 0)
+            
+#             isEnclave, cnt = 1, 1
+#             grid[i][j] = 0
+            
+#             for x, y in ((i-1, j), (i+1, j), (i, j-1), (i, j+1)):
+#                 result = dfs(x, y)
+#                 isEnclave *= result[0]
+#                 cnt += result[1]
+#             return (isEnclave, cnt)
+        
+#         ans = 0
+#         for i in range(m):
+#             for j in range(n):
+#                 if grid[i][j] == 1:
+#                     isEnclave, cnt = dfs(i, j)
+#                     if isEnclave:
+#                         ans += cnt
+#         return ans
