29c45edf335a260bdde8043ef68477f0e303035e
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-09 00:07:07 +0900

    Time: 1140 ms (22.96%), Space: 17.1 MB (59.02%) - LeetHub

--- 542-01-matrix/542-01-matrix.py
+++ 542-01-matrix/542-01-matrix.py
@@ -1,26 +1,24 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        # initialization
         m, n = len(mat), len(mat[0])
-        visited = set()
-        queue = collections.deque([])
+        q = deque([])
         for i in range(m):
             for j in range(n):
-                if mat[i][j] == 0:
-                    visited.add((i, j))
-                    queue.append((i, j))
-        dirs = [0, 1, 0, -1, 0]
+                if mat[i][j] != 0:
+                    mat[i][j] = -1 # 아직 처리되지 않은 값으로 표시
+                else:
+                    q.append((i, j)) # 값이 0인 좌표 저장
         
-        #BFS
-        while queue:
-            i, j = queue.popleft()
-            for x in range(1, len(dirs)):
-                ni, nj = i + dirs[x-1], j + dirs[x]
-                if 0 <= ni < m and 0 <= nj < n:
-                    if (ni, nj) not in visited and mat[ni][nj] == 1:
-                        visited.add((ni, nj))
-                        queue.append((ni, nj))
-                        mat[ni][nj] = mat[i][j] + 1
-                        
-                        
+        # 0인 좌표에서 네 방향으로 확인
+        DIR = [0, 1, 0, -1, 0]
+        while q:
+            x, y = q.popleft()
+            for d in range(4):
+                nx, ny = x + DIR[d], y + DIR[d+1]
+                if nx < 0 or nx >= m or ny < 0 or ny >= n or mat[nx][ny] != -1:
+                    continue
+                mat[nx][ny] = mat[x][y] + 1
+                q.append((nx, ny))
+            
+        
         return mat
