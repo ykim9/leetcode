d842404d0761047516596173cc0976de25d836e5
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-12 00:05:44 +0900

    Time: 927 ms (69.06%), Space: 14.6 MB (72.88%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -2,19 +2,16 @@ class Solution:
     def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
         x, y = entrance
         m, n = len(maze), len(maze[0])
-        reached = lambda p, q: (not p==x or not q==y) and (p==0 or q==0 or p==m-1 or q==n-1)
-        q, ans = deque(), 0
-        q.append((x, y, ans))
-        directions = [1, 0, -1, 0, 1]
+        q = deque([(x, y, 0)])
+        dirs = [1, 0, -1, 0, 1]
         while q:
             row, col, ans = q.popleft()
-            if reached(row, col):
-                return ans
             for i in range(4):
-                r, c = row+directions[i], col+directions [i+1]
-                if r<0 or c<0 or r==m or c==n or maze[r][c]=='+':
+                r, c = row+dirs[i], col+dirs[i+1]
+                if r < 0 or c < 0 or r == m or c == n or maze[r][c] == '+':
                     continue
-                
+                if (r == 0 or c == 0 or r == m - 1 or c == n - 1) and (r != x or c != y):
+                    return ans + 1
                 maze[r][c] = '+'
                 q.append((r, c, ans+1))
         return -1
