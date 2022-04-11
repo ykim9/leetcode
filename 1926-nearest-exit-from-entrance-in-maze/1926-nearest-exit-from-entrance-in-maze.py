a9582658bc0383a8300dc25abd932036b06527be
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-12 00:07:19 +0900

    Time: 784 ms (94.54%), Space: 14.5 MB (89.90%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -5,13 +5,14 @@ class Solution:
         q = deque([(x, y, 0)])
         dirs = [1, 0, -1, 0, 1]
         maze[x][y] = '+'
+        reached = lambda r, c: (r == 0 or c == 0 or r == m - 1 or c == n - 1)
         while q:
             row, col, ans = q.popleft()
             for i in range(4):
                 r, c = row+dirs[i], col+dirs[i+1]
                 if r < 0 or c < 0 or r == m or c == n or maze[r][c] == '+':
                     continue
-                if r == 0 or c == 0 or r == m - 1 or c == n - 1:
+                if reached(r, c):
                     return ans + 1
                 maze[r][c] = '+'
                 q.append((r, c, ans+1))
