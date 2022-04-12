ef308c41d2d4d5b4ef928ea36f685a6a9a3494ba
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-13 00:10:21 +0900

    Time: 205 ms (85.60%), Space: 14.4 MB (53.75%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,15 +1,29 @@
 class Solution:
-    def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def dfs(i):
-            for x in range(len(isConnected)):
-                if isConnected[i][x] == 1 and x not in visited:
-                    visited.add(x)
-                    dfs(x)
+    #DFS
+#     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+#         def dfs(i):
+#             for x in range(len(isConnected)):
+#                 if isConnected[i][x] == 1 and x not in visited:
+#                     visited.add(x)
+#                     dfs(x)
                 
-        ans = 0 
-        visited = set()
+#         ans = 0 
+#         visited = set()
+#         for i in range(len(isConnected)):
+#             if i not in visited:
+#                 dfs(i)
+#                 ans += 1
+#         return ans
+    # iterative
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        visited, ans = set(), 0
         for i in range(len(isConnected)):
             if i not in visited:
-                dfs(i)
+                s = [i]
+                while s:
+                    num = s.pop()
+                    if num not in visited:
+                        visited.add(num)
+                        s += [j for j, val in enumerate(isConnected[num]) if val == 1 and j not in visited]
                 ans += 1
         return ans
