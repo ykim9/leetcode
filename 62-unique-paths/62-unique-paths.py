32857c895024979541efe78bad1e9ba8a0645160
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-12 20:55:27 +0900

    Time: 40 ms (64.73%), Space: 14 MB (34.19%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,10 +1,11 @@
 class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         dp = [1] * n
+#         for _ in range(1, m):
+#             for j in range(1, n):
+#                 dp[j] += dp[j-1]
+#         return dp[n-1]
+    
     def uniquePaths(self, m: int, n: int) -> int:
-        @cache
-        def dfs(i, j):
-            if i < 0 or j < 0:
-                return 0
-            elif i == m - 1 or j == n - 1:
-                return 1
-            return dfs(i+1, j) + dfs(i, j+1)
-        return dfs(0, 0)
+        # 아래로 가는 m-1, 오른쪽으로 가는 n-1 가지 경우 m+n-2개 이동 방식에서 
+        return factorial(m+n-2) // (factorial(m - 1) * factorial(n - 1))
