c8c33e3fdb5566e6c00f63c21b0d9ee5630cecb4
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-04-12 21:48:17 +0900

    Time: 131 ms (54.20%), Space: 15.9 MB (26.33%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,14 +1,33 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        if not graph:
+            return []
         
-        def dfs(end, path):
-            if end == len(graph) - 1:
-                result.append(path)
-                return
-            for x in graph[end]:
-                dfs(x, path+[x])
+        n = len(graph)
+        stack = [(0, [0])]
+        res = []
+        while stack:
+            node, path = stack.pop(0)
+            if node == n - 1:
+                res.append(path)
+            
+            for x in graph[node]:
+                stack.append((x, path + [x]))
+        
+        return res
+        
+            
+    
+# dfs
+#     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+#         def dfs(end, path):
+#             if end == len(graph) - 1:
+#                 result.append(path)
+#                 return
+#             for x in graph[end]:
+#                 dfs(x, path+[x])
                 
-        result = []
-        # from 0 to n - 1 node
-        dfs(0, [0])
-        return result
+#         result = []
+#         # from 0 to n - 1 node
+#         dfs(0, [0])
+#         return result
