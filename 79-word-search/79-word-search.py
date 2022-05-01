314f440fb7878dfd15a01f2ee89aa6d52bec362d
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-05-02 00:43:24 +0900

    Time: 5476 ms (82.27%), Space: 13.9 MB (93.99%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,7 +1,7 @@
 class Solution:
     
     def exist(self, board: List[List[str]], word: str) -> bool:
-        def dfs(x, y, pos=0, visited=set()):
+        def dfs(x, y, visited, pos=0):
             if x < 0 or x >= m or y < 0 or y >= n or word[pos] != board[x][y] or (x, y) in visited:
                 return False
             
@@ -10,15 +10,16 @@ class Solution:
 
             visited.add((x, y))
             for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                if dfs(x + dx, y + dy, pos + 1, visited):
+                if dfs(x + dx, y + dy, visited, pos+1):
                     return True
             visited.remove((x, y))
             return False
-                
+        
+        visited = set()
         m, n = len(board), len(board[0])
         for i in range(m):
             for j in range(n):
-                if dfs(i, j):
+                if dfs(i, j, visited):
                     return True
         return False
                     
