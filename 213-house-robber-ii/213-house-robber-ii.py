20cc59fdfaf3c22f8356c2b29ac5a8ce11424ce1
ykim9 <102279938+ykim9@users.noreply.github.com>
2022-05-02 23:59:11 +0900

    Time: 43 ms (54.20%), Space: 14 MB (6.90%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         def do_rob(i, j):
-            rob, not_rob = 0, 0
+            last, now = 0, 0
             for x in range(i, j):
                 num = nums[x]
-                rob, not_rob = not_rob + num, max(rob, not_rob)
-            return max(rob, not_rob)
+                last, now = now, max(last + num, now)
+            return now
         if len(nums) == 1:
             return nums[0]
         return max(do_rob(1, len(nums)), do_rob(0, len(nums)-1))
