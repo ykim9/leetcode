bd38c78b933768ac58394a7e041a03d7ed767b9f
ykim9 <yeonsik.kim2020@gmail.com>
2022-06-22 22:56:17 +0900

    Time: 59 ms (24.53%), Space: 13.9 MB (65.06%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,14 +1,14 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> int:
-        ans = 0
-        for n in collections.Counter(s).values():
-            ans += (n // 2) * 2
-            if ans % 2 == 0 and n % 2 == 1:
-                ans += 1
-        return ans
+    # def longestPalindrome(self, s: str) -> int:
+    #     ans = 0
+    #     for n in collections.Counter(s).values():
+    #         ans += (n // 2) * 2
+    #         if ans % 2 == 0 and n % 2 == 1:
+    #             ans += 1
+    #     return ans
     
     # count odds
-#     def longestPalindrome2(self, s: str) -> int:
-#         odds = sum([n % 2 == 1 for n in collections.Counter(s).values()])
-#         return len(s) - odds + bool(odds)
+    def longestPalindrome(self, s: str) -> int:
+        odds = sum([n % 2 == 1 for n in collections.Counter(s).values()])
+        return len(s) - odds + bool(odds)
     
